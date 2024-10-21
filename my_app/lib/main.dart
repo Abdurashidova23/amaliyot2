c91b10ba248e9c7960644fb5cf09465d3a697579
Abdurashidova23 <marjonafozilovna@gmail.com>
2024-10-21 23:35:08 +0500

    First commit

---
+++ my_app/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ListView Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<String> items =
+      List<String>.generate(100, (index) => 'Item $index');
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Example'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          bool toq = index % 2 == 0;
+          return ListTile(
+            title: Text(items[index],),
+          );
+        },
+      ),
+    );
+  }
+}
